6ec10f3c533be223ca62e5b5df8072d2bb5df5fe
jh5201314 <75761055+jh5201314@users.noreply.github.com>
2026-07-07 15:16:40 +0800

    Update HomeSwiper.vue

--- src/views/home/childComps/HomeSwiper.vue
+++ src/views/home/childComps/HomeSwiper.vue
@@ -27,3 +27,32 @@ export default {
 }
 
 </script>
+<template>
+  <swiper>
+      <swiper-item v-for="item in banners" :key="item.swiper">
+        <a href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
+</template>
+<script>
+import Swiper from 'components/common/swiper/Swiper.vue'
+import SwiperItem from 'components/common/swiper/SwiperItem.vue'
+export default {
+  name:'HomeSwiper',
+  props:{
+    banners:{
+      type:Array,
+      default(){
+        return[]
+      }
+    }
+  },
+  components:{
+  Swiper,
+  SwiperItem
+}
+}
+
+</script>
